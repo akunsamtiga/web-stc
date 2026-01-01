e8f30da3ee6cc5de7a13d5ad6b1bf99844a2e66e
aryasis87 <aryasis87@gmail.com>
2026-01-01 22:25:54 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { Dashboard } from './pages/Dashboard';
 import { Users } from './pages/Users';
 import { Admins } from './pages/Admins';
 import { Settings } from './pages/Settings';
-import { NotFound } from './pages/NotFound';
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { user, loading } = useAuth();
@@ -43,8 +42,6 @@ function App() {
             <Route path="admins" element={<Admins />} />
             <Route path="settings" element={<Settings />} />
           </Route>
-          {/* Add 404 route - catches all unmatched paths */}
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <Toaster
